ff50505d831d1eabdc9ed01ac99e0b9b0bbbc739
Djeufack kafack berios arol <arolddjeufack@gmail.com>
2026-06-04 16:33:23 +0100

    feat: update Navbar component with logo and styling improvements

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './navbar.css';
+import logo from '../assets/logo_to_do_sans_bg.png';
 
 function Navbar() {
   const navigate = useNavigate();
@@ -18,10 +19,13 @@ function Navbar() {
 
   return (
     <nav className="navbar">
-      <h1>Ma To-Do List</h1>
+      <div className="logo-title">
+        <img src={logo} alt="Logo" className="logo" />
+        <h1>To-Do App</h1>
+      </div>
       <div className="links">
         <button onClick={handleLogout} className="logout-btn">
-          Déconnexion
+          <icon className="fas fa-sign-out-alt"></icon> &nbsp;Déconnexion
         </button>
       </div>
     </nav>
